4c28903334f7349455cba387e1aff4a9d758ce88
andreamusso96 <andreamusso96@gmail.com>
2023-11-22 15:14:10 +0100

    Added Dataset class to deal with DataArray for different cities

--- src/mobile_traffic/aggregate.py
+++ src/mobile_traffic/aggregate.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Callable
 
 import numpy as np
 import xarray as xr
@@ -10,6 +10,28 @@ from .load import load_traffic_data
 from .clean import remove_times_outside_range, remove_nights_when_traffic_data_is_noisy
 
 
+class Dataset:
+    def __init__(self, data: Dict[City, xr.DataArray]):
+        self.data = data
+
+    def __getitem__(self, city: City) -> xr.DataArray:
+        return self.data[city]
+
+    def cities(self) -> List[City]:
+        return list(self.data.keys())
+
+    def group_by(self, group: str, func: Callable) -> 'Dataset':
+        return Dataset(data={city: data.groupby(group=group).reduce(func=func) for city, data in self.data.items()})
+
+    def save(self, folder_path: str):
+        for city, data in self.data.items():
+            data.to_netcdf(f'{folder_path}/{city.value}.nc')
+
+    def load(self, folder_path: str, city: List[City] = None):
+        city = city if city is not None else [c for c in City]
+        self.data = {c: xr.open_dataset(f'{folder_path}/{c.value}.nc').to_array().squeeze() for c in city}
+
+
 def day_time_to_datetime_index(xar: xr.DataArray) -> xr.DataArray:
     new_index = np.add.outer(xar.indexes[TrafficDataDimensions.DAY.value], xar.indexes[TrafficDataDimensions.TIME.value]).flatten()
     datetime_xar = xar.stack(datetime=(TrafficDataDimensions.DAY.value, TrafficDataDimensions.TIME.value), create_index=False)
@@ -17,22 +39,20 @@ def day_time_to_datetime_index(xar: xr.DataArray) -> xr.DataArray:
     return datetime_xar
 
 
-def get_night_traffic_by_tile_service_time_city(traffic_type: TrafficType, start_night: time, end_night: time, city: List[City] = None, service: List[Service] = None, remove_noisy_nights: bool = True):
+def get_night_traffic_by_tile_service_time_city(traffic_type: TrafficType, start_night: time, end_night: time, city: List[City] = None, service: List[Service] = None, remove_noisy_nights: bool = True) -> Dataset:
     city = city if city is not None else [c for c in City]
     service = service if service is not None else [s for s in Service]
-
-    traffic_data = [get_night_traffic_city_by_tile_service_time(city=c, traffic_type=traffic_type, start_night=start_night, end_night=end_night, service=service, remove_noisy_nights=remove_noisy_nights, expand_city_dimension=True) for c in tqdm(city)]
-    traffic_data = xr.concat(traffic_data, dim=TrafficDataDimensions.CITY.value)
-    return traffic_data
+    traffic_data = {c: get_night_traffic_city_by_tile_service_time(city=c, traffic_type=traffic_type, start_night=start_night, end_night=end_night, service=service, remove_noisy_nights=remove_noisy_nights) for c in tqdm(city)}
+    return Dataset(data=traffic_data)
 
 
-def get_night_traffic_by_tile_service_city(traffic_type: TrafficType, start_night: time, end_night: time, city: List[City] = None, service: List[Service] = None, remove_noisy_nights: bool = True):
+def get_night_traffic_by_tile_service_city(traffic_type: TrafficType, start_night: time, end_night: time, city: List[City] = None, service: List[Service] = None, remove_noisy_nights: bool = True) -> Dataset:
     traffic_data = get_night_traffic_by_tile_service_time_city(traffic_type=traffic_type, start_night=start_night, end_night=end_night, city=city, service=service, remove_noisy_nights=remove_noisy_nights)
-    traffic_data = traffic_data.groupby(group='time').sum()
+    traffic_data = traffic_data.group_by(group='time', func=np.sum)
     return traffic_data
 
 
-def get_night_traffic_city_by_tile_service_time(city: City, traffic_type: TrafficType, start_night: time, end_night: time, service: List[Service], remove_noisy_nights: bool = True, expand_city_dimension: bool = False):
+def get_night_traffic_city_by_tile_service_time(city: City, traffic_type: TrafficType, start_night: time, end_night: time, service: List[Service], remove_noisy_nights: bool = True):
     traffic_data_city = []
     for i in range(0, len(service), 5):
         service_ = service[i:i + 5]
@@ -46,8 +66,4 @@ def get_night_traffic_city_by_tile_service_time(city: City, traffic_type: Traffi
         traffic_data_city.append(traffic_data_service)
 
     traffic_data_city = xr.concat(traffic_data_city, dim=TrafficDataDimensions.SERVICE.value)
-
-    if expand_city_dimension:
-        traffic_data_city = traffic_data_city.expand_dims(dim={TrafficDataDimensions.CITY.value: [city.value]})
-
     return traffic_data_city
